034b7d8f312e8bbe838b54ace8db9311c21a5f48
Madalina Melinte <Madalina.Melinte@ness.com>
2015-10-09 16:23:28 +0300

    StackOverflowVisualiser project commit

---
+++ StackOverflowVisualiser/StackOverflowVisualiser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StackOverflowVisualiser
+//
+//  Created by Madalina Melinte on 09/10/15.
+//  Copyright © 2015 Madalina Melinte. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
