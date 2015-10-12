f16de7368d210829139f0f219e53e789fd1545ed
Madalina Melinte <Madalina.Melinte@ness.com>
2015-10-12 09:53:12 +0300

    added StackExchange class  with URLForQuery method

---
+++ StackOverflowVisualiser/StackOverflowVisualiser/StackExchange.h
@@ -0,0 +1,13 @@
+//
+//  StackExchange.h
+//  StackOverflowVisualiser
+//
+//  Created by Madalina Melinte on 09/10/15.
+//  Copyright © 2015 Madalina Melinte. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface StackExchange : NSObject
+
+@end
